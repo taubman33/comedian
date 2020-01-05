2f5f0e0a46f142f0c30258e75c1f215f92823b14
JT <taubman33@gmail.com>
2020-01-05 11:31:59 -0500

    getting better

--- js/main.js
+++ js/main.js
@@ -17,18 +17,21 @@ async function getData () {
                 'Content-Type': 'application/json'
             }
         }
+        
+        
         let result = await axios(options)
+        let jokeReturn = document.querySelector('.jokeReturn')
+        jokeReturn.innerHTML = ''
 
         let joke = result.data.joke
-        console.log(joke,  "+ Part 2")
+        console.log("Part 2 + ", joke )
         // let splitJoke = joke.split('?')
         // console.log(splitJoke)
 
-        
         let jokeName = document.createElement('jokeReturn')
+        jokeReturn.innerHTML = ''
         jokeName.innerHTML = (joke)
         
-        let jokeReturn =  document.querySelector('.jokeReturn')
         jokeReturn.append(joke)
         console.log (`Part 4 +  ${jokeInput} `)
         
@@ -36,9 +39,23 @@ async function getData () {
         console.log(error);
    }  
 }
-getData()
+
+
+//making a new city Object each time we enter and submit info
+const newJoke = function(event) {
+    //prevents site from reloading with each click
+    event.preventDefault()
+    let jokeReturn = jokeInput.value
+    //calling our getWeather function here
+    getData(jokeReturn)
+    console.log(jokeReturn)
+}
+
+
+
 getJoke.addEventListener('click', getData)
-getData.preventDefault()
+
+
 
 
 

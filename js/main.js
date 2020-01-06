913b19e4de0d417ef4d6ac3d3a7d946ec811dab2
JT <taubman33@gmail.com>
2020-01-06 10:10:04 -0500

    working on search and drop bar

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 
 let jokeInput = document.querySelector('#inputBar');
 let getJoke = document.querySelector('#getJoke');
-const jokeUrl = `https://icanhazdadjoke.com/`;
-console.log (`Part 1 + jokeInput ${jokeInput} + jokeURL ${jokeUrl} `);
+const jokeUrl = `https://icanhazdadjoke.com`;
+// const jokeUrl = `https://icanhazdadjoke.com/search?term=${jokeInput.value}`;
+console.log (`Part 1 + jokeURL ${jokeUrl} `);
 
 //async function . using the async function to retrieve info 
+
+
 
 async function getData () {
     try {
@@ -17,44 +20,49 @@ async function getData () {
                 'Content-Type': 'application/json'
             }
         }
-        
-        
         let result = await axios(options)
-        let jokeReturn = document.querySelector('.jokeReturn')
-        jokeReturn.innerHTML = ''
 
         let joke = result.data.joke
         console.log("Part 2 + ", joke )
+
+
+
         // let splitJoke = joke.split('?')
         // console.log(splitJoke)
+        // let splitJoke = joke.split('?', '.')
+        // console.log("part 3  +",  splitJoke)
+        // splitJoke. if has ? or . adds <br> to html
 
         let jokeName = document.createElement('jokeReturn')
-        jokeReturn.innerHTML = ''
         jokeName.innerHTML = (joke)
         
+        let jokeReturn =  document.querySelector('.jokeReturn')
+        jokeReturn.innerHTML = ''
         jokeReturn.append(joke)
-        console.log (`Part 4 +  ${jokeInput} `)
+        console.log (`Part 4 `)
+
+
         
         } catch (error) {
         console.log(error);
    }  
 }
-
-
-//making a new city Object each time we enter and submit info
-const newJoke = function(event) {
-    //prevents site from reloading with each click
-    event.preventDefault()
-    let jokeReturn = jokeInput.value
-    //calling our getWeather function here
-    getData(jokeReturn)
-    console.log(jokeReturn)
-}
-
-
-
+// //making a new city Object each time we enter and submit info
+// const newJoke = function(event) {
+//     //prevents site from reloading with each click
+//     event.preventDefault()
+//     let jokeReturn = jokeInput.value
+//     //calling our getWeather function here
+//     getData(jokeReturn)
+//     console.log(jokeReturn)
+// }
 getJoke.addEventListener('click', getData)
 
+
+// Dropdown 
+
+// let jokeTopic = document.getElementById('dropBtn');
+// jokeTopic = `${jokeTopic}
 
 
 

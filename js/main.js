c4847f50d59936457c676f0228fd21f791d63772
JT <taubman33@gmail.com>
2020-01-07 09:23:26 -0500

    working on styling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 // JT GA API Lab
 
-
 let jokeInput = document.querySelector('#inputBar');
 let getJoke = document.querySelector('#getJoke');
-// let query = '';
+
 const jokeUrl = `https://icanhazdadjoke.com/search?term=${jokeInput.value}`;
 console.log (`Part 1 + jokeURL ${jokeUrl} `);
 
 async function getData (e) {
     e.preventDefault()
     const jokeList = document.querySelector(`#returnJoke`)
-    // while (jokeList.lastChild) {
-    //     jokeList.removeChild(jokeList.lastChild)
-    // }
 
     try {
         const searchTerm = document.querySelector('#inputBar').value
@@ -29,48 +25,14 @@ async function getData (e) {
         let result = await axios.get(URL, options)
         const jokeData = result.data.results
         console.log("Part 2 + ", jokeData)
-
-     
         
-            const jokeList = document.querySelector('.jokeReturn')
-            let newJoke = document.createElement('li')
-            jokeList.innerHTML = jokeData[0].joke
-            // [0].joke            
-            
-            // newJoke.innerHTML = object.joke
-            // jokeList.append(newJoke.)
+        const jokeList = document.querySelector('.jokeReturn')
+        let newJoke = document.createElement('li')
+        const randomNum = Math.floor(Math.random() * 10);
+        jokeList.innerHTML = jokeData[`${randomNum}`].joke
+        
         } catch (error) {
         console.log(error);
      }  
     }
     getJoke.addEventListener('click', getData);
-    
-
-
-    
-
-
-// let jokeName = document.createElement('jokeReturn')
-        // jokeName.innerHTML = (joke)
-        
-        // let jokeReturn =  document.querySelector('.jokeReturn')
-        // jokeReturn.innerHTML = ''
-        // jokeReturn.append(joke)
-        // console.log (`Part 3 + ${query}` )
-
-
-    //once we get search bar working, making sure only One joke is appended into HTML
-    // document.append -> value [0]
-    
-
-
-        // splitting joke into 2 lines rather than 1 block of text -> some jokes have 3 lines
-        // let splitJoke = joke.split('?', '.')
-        // console.log("split joke + ", splitJoke)
-        // function splitStr(joke) { 
-        //     var string = str.split("*"); 
-        //     console.log(string); 
-        // } 
-        // splitJoke. if has ? or . adds <br> to html       
-
-        
